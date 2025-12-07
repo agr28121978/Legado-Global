72b28c37cb8b4901051f3ba5f74b311e8f70ec78
agr28121978 <agr28121978@gmail.com>
2025-12-07 14:41:40 +0100

    Add files via upload

---
+++ jss/main.js
@@ -0,0 +1,7 @@
+
+/* =============================================
+   LEGADO DEPORTIVO GLOBAL 3D
+   main.js - Archivo JavaScript Principal
+   ============================================= */
+
+// Esperar a que el DOM esté completamente cargado
